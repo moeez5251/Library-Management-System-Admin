b357e49587c2983ac5ef4d72c15da37da47522fa
moeez5251 <moeez66656@gmail.com>
2025-04-19 14:39:39 +0500

    Updating Backend

--- app/backend/controller/authController.js
+++ app/backend/controller/authController.js
@@ -7,20 +7,16 @@ exports.login = async (req, res) => {
     console.log(req.body);
   try {
     const [users] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
-
     if (users.length === 0) return res.status(400).json({ message: 'No User Found' });
-
+    
     const user = users[0];
     const isMatch = await bcrypt.compare(password, user.password);
+    console.log(process.env.JWT_SECRET);
     if (!isMatch) return res.status(401).json({ message: 'Invalid password' });
-
-    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
-      expiresIn: '1d',
-    });
-
+    
+    
     res.json({
       message: 'Login successful',
-      token,
       user: {  name: user.name, email: user.email },
     });
   } catch (error) {
--- app/backend/models/db.js
+++ app/backend/models/db.js
@@ -6,6 +6,9 @@ const pool = mysql.createPool({
   user: 'sql8773768',
   password: 'RJi1vkUMZw',
   database: 'sql8773768',
+  port:3306,
+  connectTimeout: 10000, // 10 seconds
+  connectionLimit: 10, // Freesqldatabase.com may limit concurrent connections
 });
 
 module.exports = pool;
