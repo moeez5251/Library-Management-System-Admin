440a8b3626528887b2cc788dfe2008d645365047
moeez5251 <moeez66656@gmail.com>
2025-07-14 15:47:13 +0500

    Refactor addbook function to improve user creation process and update email login link

--- app/backend/controller/lendersControllers.js
+++ app/backend/controller/lendersControllers.js
@@ -53,7 +53,6 @@ exports.addbook = async (req, res) => {
         if (existingUserResult.recordset[0].count > 0) {
             return res.status(400).json({ error: 'User with this email already exists' });
         }
-
         const userId = `${data.Lendername[0].toUpperCase()}${uuidv4().replace(/-/g, "").slice(0, 8)}`;
         const password = uuidv4().replace(/-/g, "").slice(0, 8) + uuidv4().replace(/-/g, "").slice(0, 8);
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -64,10 +63,29 @@ exports.addbook = async (req, res) => {
             .input('Email', data.Email)
             .input('Role', data.Role)
             .input('Membership_Type', 'English')
-            .input('password', hashedPassword)
+            .input('Password', hashedPassword)
             .input('Cost', data.Price)
             .input('Status', 'Active')
-            .query('INSERT INTO users (User_id, User_Name, Email, Role, Membership_Type, Password,Cost,Status) VALUES (@User_id, @User_Name, @Email, @Role, @Membership_Type, @Password,@Cost,@Status)');
+            .query(`
+                INSERT INTO users (User_id, User_Name, Email, Role, Membership_Type, Password, Cost, Status)
+                OUTPUT INSERTED.User_id
+                VALUES (@User_id, @User_Name, @Email, @Role, @Membership_Type, @Password, @Cost, @Status);
+            `);
+        const insert = await pool
+            .request()
+            .input('user_id', userId)
+            .input('Name', data.Lendername)
+            .input('PhoneNumber', data.PhoneNumber)
+            .input('BookTitle', data.BookTitle)
+            .input('Author', data.Author)
+            .input('Category', data.Category)
+            .input('IssuedDate', data.IssuedDate)
+            .input('DueDate', data.DueDate)
+            .input('CopiesLent', data.CopiesLent)
+            .input('FinePerDay', data.Fine)
+            .input('Price', data.Price)
+            .query(`INSERT INTO borrower (user_id, Name, PhoneNumber, BookTitle, Author, Category, IssuedDate, DueDate, CopiesLent, FinePerDay, Price) VALUES (@user_id, @Name, @PhoneNumber, @BookTitle, @Author, @Category, @IssuedDate, @DueDate, @CopiesLent, @FinePerDay, @Price)`);
+        
         const text = `
         Hello ${data.Lendername},
 
@@ -78,8 +96,9 @@ exports.addbook = async (req, res) => {
 
         Please keep this information secure.
 
-        You can log in at: https://yourdomain.com/login
+        You can log in at: https://xlms-admin.netlify.app
 
+        If you have any questions or need further assistance, please don't hesitate to contact us.
         Thank you,
         XLMS
         `;
@@ -141,7 +160,7 @@ exports.addbook = async (req, res) => {
 
             <p>Please keep this information safe and do not share it with anyone.</p>
 
-            <a href="https://yourdomain.com/login" class="button">Log In Now</a>
+            <a href="https://xlms-admin.netlify.app" class="button">Log In Now</a>
 
             <p class="footer">
             If you did not request this account, please contact our support team immediately.<br><br>
@@ -152,11 +171,14 @@ exports.addbook = async (req, res) => {
         </html>
         `;
 
-        sendEmail(data.Email, 'Login Credentials', text, html);
+        // sendEmail(data.Email, 'Thank you for creating an account with XLMS', text, html);
 
-        res.status(200).json({ message: 'User created successfully' });
+
+        res.status(200).json({ message: 'User created successfully', result: result });
+
     }
     catch (e) {
+        console.log(e);
         res.status(500).json({ message: 'Server error', error: e });
     }
 }
