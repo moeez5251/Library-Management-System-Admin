f14c265365085b320735ff739cd405021143a1c3
moeez5251 <moeez66656@gmail.com>
2025-07-24 19:15:15 +0500

    Fixing Middleware

--- middleware.js
+++ middleware.js
@@ -1,28 +1,29 @@
 import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
-const JWT_SECRET = new TextEncoder().encode(process.env.JWT); 
+const JWT_SECRET = new TextEncoder().encode(process.env.JWT);
 
 export async function middleware(request) {
-  const { pathname } = request.nextUrl;
+    const { pathname } = request.nextUrl;
 
-  if (pathname.startsWith('/admin')) {
-    const token = request.cookies.get('jwt')?.value;
-    if (!token) {
-      const url = request.nextUrl.clone();
-      url.pathname = '/';
-      return NextResponse.redirect(url);
+    if (pathname.startsWith('/admin')) {
+        const token = request.cookies.get('jwt')?.value;
+        console.log(JWT_SECRET, token);
+        if (!token) {
+            const url = request.nextUrl.clone();
+            url.pathname = '/';
+            return NextResponse.redirect(url);
+        }
+
+        try {
+            await jwtVerify(token, JWT_SECRET);
+        } catch (err) {
+            console.error('JWT verification failed:', err);
+            const url = request.nextUrl.clone();
+            url.pathname = '/';
+            return NextResponse.redirect(url);
+        }
     }
 
-    try {
-      await jwtVerify(token, JWT_SECRET);
-    } catch (err) {
-      console.error('JWT verification failed:', err);
-      const url = request.nextUrl.clone();
-      url.pathname = '/';
-      return NextResponse.redirect(url);
-    }
-  }
-
-  return NextResponse.next();
+    return NextResponse.next();
 }
