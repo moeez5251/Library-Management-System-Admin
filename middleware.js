9d805e7ac949124bb208c942504d53806636cd2c
moeez5251 <moeez66656@gmail.com>
2025-07-30 16:47:13 +0500

    Updating Some bugs

--- middleware.js
+++ middleware.js
@@ -7,7 +7,7 @@ export function middleware(request) {
     if (token) {
         console.log(token);
         res.headers.set("x-debug-token", token); // 🪵 log it in browser devtools
-        return NextResponse.redirect(new URL('/admin', request.url));
+       
     } else {
         res.headers.set("x-debug-token", "undefined");
         return NextResponse.redirect(new URL('/', request.url));
@@ -16,6 +16,6 @@ export function middleware(request) {
     return res;
 }
 export const config = {
-  matcher: '/admin/:path*', // matches /admin, /admin/anything, /admin/anything/more
+  matcher: '/admin/:path*', 
 };
 
