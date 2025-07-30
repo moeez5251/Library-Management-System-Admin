bab3ba6ee81b3b2e3cef8a6f4de39b6d5702b1a2
moeez5251 <moeez66656@gmail.com>
2025-07-30 16:03:39 +0500

    Upating Coookies settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://library-management-system-1-os0y.onrender.com/api/:path*'
-      }
-    ];
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
